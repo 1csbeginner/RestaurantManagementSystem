ee559d9df4767cd76ae50cb5c3dc3514cc36bbdc
zq <1615027706@qq.com>
2024-11-09 16:47:35 +0800

    login update(add register)

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -5,7 +5,7 @@
         <img src="../assets/logo.svg" alt="logo" />
       </div>
       <!--登录表单-->
-      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules"
+      <el-form v-if="isLoginMode" ref="loginFormRef" :model="loginForm" :rules="loginFormRules"
        class="login_form" label-width="0px">
         <!--用户名-->
         <el-form-item prop="name">
@@ -27,22 +27,60 @@
         <!--登录按钮-->
         <el-form-item class="btns">
           <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="info" @click="resetLoginForm">重置</el-button>
+          <el-button type="info" @click="resetForm">重置</el-button>
+          <el-button type="primary" link @click="toggleMode">没有账号？立即注册</el-button>
+        </el-form-item>
+      </el-form>
+
+      <el-form v-else ref="registerFormRef" :model="registerForm" :rules="registerFormRules"
+       class="login_form" label-width="0px">
+        <!--用户名-->
+        <el-form-item prop="name">
+          <el-input v-model="registerForm.name" placeholder="用户名">
+            <!--引入图标-->
+            <template #prefix>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <!--密码-->
+        <el-form-item prop="password">
+          <el-input v-model="registerForm.password" type="password" placeholder="密码">
+          <template #prefix>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+        </el-form-item>
+        <!--确认密码-->
+        <el-form-item prop="confirmPassword">
+         <el-input v-model="registerForm.confirmPassword" type="password" placeholder="确认密码">
+         <template #prefix>
+             <el-icon><Lock /></el-icon>
+           </template>
+         </el-input>
+        </el-form-item>
+        <!--注册按钮-->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="register">注册</el-button>
+          <el-button type="info" @click="resetForm">重置</el-button>
+          <el-button type="primary" link @click="toggleMode">已有账号？立即登录</el-button>
         </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
+
 <script>
-import { ElMessage } from 'element-plus';
+
 
 export default {
   data(){
     return {
+      isLoginMode: true,
       //登录表单数据
       loginForm: {
-        name: 'zz',
-        password: '123123'
+        name: '',
+        password: ''
       },
       //验证规则
       loginFormRules: {
@@ -53,14 +91,55 @@ export default {
         password:[
           {required:true, message:'请输入密码', trigger:'blur'}
         ]
+      },
+      //注册表单数据
+      registerForm: {
+        name: '',
+        password: '',
+        confirmPassword: ''
+      },
+      registerFormRules: {
+        name:[
+          {required:true, message:'请输入用户名', trigger:'blur'},
+          {min:3, max:10, message:'长度在3到10个字符', trigger:'blur'}
+        ],
+        password:[
+          {required:true, message:'请输入密码', trigger:'blur'}
+        ],
+        confirmPassword:[
+          {required:true, message:'请输入确认密码', trigger:'blur'},
+          {validator: (rule, value, callback) => {
+              if (value !== this.registerForm.password) {
+                callback(new Error('两次输入的密码不一致'));
+              } else {
+                callback();
+              }
+            },
+            trigger: 'blur'
+          }
+        ]
       }
     }
   },
   methods:{
+    toggleMode(){
+      this.isLoginMode = !this.isLoginMode;
+      this.loginForm = { name: '', password: '' };
+      this.registerForm = { name: '', password: '', confirmPassword: '' };
+      //重置表单
+      this.resetForm();
+    },
     //重置表单
-    resetLoginForm(){
-      // console.log(this) $refs有loginFormRef，所以可以调用resetFields方法
-      this.$refs.loginFormRef.resetFields()
+     resetForm() {
+      if (this.isLoginMode) {
+        if (this.$refs.loginFormRef) {
+          this.$refs.loginFormRef.resetFields();
+        }
+      } else {
+        if (this.$refs.registerFormRef) {
+          this.$refs.registerFormRef.resetFields();
+        }
+      }
     },
     login(){
       this.$refs.loginFormRef.validate(async valid => {
@@ -84,6 +163,41 @@ export default {
           return
         }
       })
+    },
+    register() {
+      this.$refs.registerFormRef.validate(async valid => {
+        // 验证成功返回 true，验证失败返回 false
+        if (valid) {
+          // 只上传用户名和密码
+          const userData = {
+            name: this.registerForm.name,
+            password: this.registerForm.password
+          };
+
+          // 发送注册请求
+          const { data: res } = await this.$http.post("user/add-one-user", userData);
+          console.log(res);
+
+          // 检查后端返回的数据，判断注册是否成功
+          if (res.code === '00000') {
+            ElMessage({
+              message: '注册成功',
+              type: 'success'
+            });
+
+            // 保存登录状态
+            window.sessionStorage.setItem("isLogin", 'true');
+
+            // 跳转到首页
+            this.$router.push("/home");
+          } else {
+            ElMessage.error("注册失败");
+          }
+        } else {
+          // 验证未通过
+          return;
+        }
+      });
     }
   }
 }
